19d051a0a5467679802add281197ca1d905b82eb
Hollajay <127396029+Hollajay@users.noreply.github.com>
2024-07-03 00:11:37 +0100

    corrections

--- src/component/Task.js
+++ src/component/Task.js
@@ -1,21 +1,17 @@
-import React from 'react'
+import React from 'react';
 import './Task.css';
+import { MdOutlineDeleteForever, MdEditNote } from 'react-icons/md';
 
-import {MdOutlineDeleteForever,MdEditNote} from 'react-icons/md';
-
-
-const Task = ({task,index}) => {
+const Task = ({ task, index, delTaskHandler }) => {
   return (
     <div className='task'>
-        <p>{index +1}<b>{task.name}</b> <b>{task.createdAt}</b></p>
-        <div className='task-icons'>
-            
-            <MdEditNote />
-            <MdOutlineDeleteForever/>
-        </div>
-       
+      <p>{index + 1} <b>{task.name}</b> <b>{new Date(task.createdAt).toLocaleString()}</b></p>
+      <div className='task-icons'>
+        <MdEditNote />
+        <MdOutlineDeleteForever onClick={() => delTaskHandler(task._id)} />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Task
+export default Task;
--- src/component/TaskList.js
+++ src/component/TaskList.js
@@ -1,53 +1,53 @@
-import React, { useEffect, useRef, useState } from 'react'
-import Form from './Form'
-import Task from './Task'
-import axios from 'axios'
+import React, { useEffect, useState } from 'react';
+import Form from './Form';
+import Task from './Task';
+import axios from 'axios';
+
 const TaskList = () => {
-const [task, setTask] = useState([])
+  const [tasks, setTasks] = useState([]);
 
-  const URL = 'http://localhost:8500'
-  const checkServer = async()=>{
-  try {
-    const data = await axios.get(`${URL}/api/task`)
-     setTask(data.data)
-    console.log(data);
+  const URL = 'http://localhost:8500';
 
-   
-  } catch (error) {
-    alert(error.message)
-  }
-  }
+  const checkServer = async () => {
+    try {
+      const data = await axios.get(`${URL}/api/task`);
+      setTasks(data.data);
+      console.log(data);
+    } catch (error) {
+      alert(error.message);
+    }
+  };
 
+  useEffect(() => {
+    checkServer();
+  }, []);
 
-
-
-useEffect(() => {
-   checkServer()
-}, [])
+  // const delTaskHandler = async (id) => {
+  //   try {
+  //     await axios.delete(`${URL}/api/task/${id}`);
+  //     setTasks(tasks.filter(task => task._id !== id));
+  //   } catch (error) {
+  //     alert('Error deleting task: ' + error.message);
+  //   }
+  // };
 
   return (
     <div>
-       <h1 className='--center-all --text-purple'>MANAGER</h1> 
-       <div className="--flex-between --pb">
-          <h3>
-            <b>Total Tasks:</b> 0
-          </h3>
-          <h3>
-            <b>Completed Tasks:</b> 0
-          </h3>
-        </div>
-    {
-      task.map((task, index) =>{
-return(
-  <Task task={task} index={index} />
-)
-      })
-    }
-      
-       <Form/>
-       
+      <h1 className='--center-all --text-purple'>MANAGER</h1>
+      <div className="--flex-between --pb">
+        <h3>
+          <b>Total Tasks:</b> {tasks.length}
+        </h3>
+        <h3>
+          <b>Completed Tasks:</b> {tasks.filter(task => task.completed).length}
+        </h3>
+      </div>
+      {tasks.map((task, index) => (
+        <Task key={task._id} task={task} index={index}  />
+      ))}
+      <Form />
     </div>
-  )
-}
+  );
+};
 
-export default TaskList
+export default TaskList;
